a502a197383f51f27ff0357ad8a8114f1080b3dc
Varun Malik <varun1malik@gmail.com>
2018-07-23 01:40:56 +0530

    aggregate solution

--- AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
+++ AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
@@ -1,14 +1,36 @@
 using System;
 using Xunit;
+using AggregateGDPPopulation;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AggregateGDPPopulation.Tests
 {
-    public class UnitTest1
+    public class AggregateDataTest
     {
         [Fact]
-        public void Test1()
+        public async void ShouldBeSameAsExpectedOutput()
         {
-
+            Aggregate classagg = new Aggregate();
+            await classagg.AggregateGDP2012Population(@"../../../../AggregateGDPPopulation/data/datafile.csv");
+            string actualFile = File.ReadAllText("../../../expected-output.json");
+            string expectedFile = File.ReadAllText(Environment.CurrentDirectory + @"/output/output.json");
+            Dictionary<string, GDPPopulation> actual = JsonConvert.DeserializeObject<Dictionary<string, GDPPopulation>>(actualFile);
+            Dictionary<string, GDPPopulation> expected = JsonConvert.DeserializeObject<Dictionary<string, GDPPopulation>>(expectedFile);
+            foreach (var key in actual.Keys)
+            {
+                if (expected.ContainsKey(key))
+                {
+                    Assert.Equal(actual[key].GDP_2012, expected[key].GDP_2012);
+                    Assert.Equal((double)actual[key].POPULATION_2012, (double)expected[key].POPULATION_2012);
+                }
+                else
+                {
+                    Assert.True(false);
+                }
+            }
         }
     }
 }
--- AggregateGDPPopulation/AggregateGDPPopulation.cs
+++ AggregateGDPPopulation/AggregateGDPPopulation.cs
@@ -1,8 +1,88 @@
 ﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AggregateGDPPopulation
 {
-    public class Class1
+    public class Aggregate
     {
+        string outputFileName = "/output.json";
+        string outputFilePath = Environment.CurrentDirectory + @"/output";
+        string countryJsonPath = @"../../../../AggregateGDPPopulation/data/country.json";
+        public async Task<string> ReadAsync(string filePath)
+        {
+            string result;
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                result = await reader.ReadToEndAsync();
+            }
+            return result;
+        }
+
+        public async Task WriteFile(Dictionary<String, GDPPopulation> dictionaryOfObjects)
+        {
+            if (!Directory.Exists(outputFilePath))
+            {
+                Directory.CreateDirectory(outputFilePath);
+            }
+            string JsonObject = JsonConvert.SerializeObject(dictionaryOfObjects);
+            using (StreamWriter writer = new StreamWriter(outputFilePath + outputFileName))
+            {
+                await writer.WriteAsync(JsonObject);
+            }
+        }
+        public async Task AggregateGDP2012Population(string filePath)
+        {
+            var csvfile = ReadAsync(filePath);
+            var jsonfile = ReadAsync(countryJsonPath);
+            await Task.WhenAll(csvfile, jsonfile);
+            var Mapper = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonfile.Result);
+            string[] CsvProcessed = csvfile.Result.Replace("\"", String.Empty).Trim().Split('\n');
+            string[] headers = CsvProcessed[0].Split(',');
+            int populationIndex = Array.IndexOf(headers, "Population (Millions) 2012");
+            int gdpIndex = Array.IndexOf(headers, "GDP Billions (USD) 2012");
+            int countriesIndex = Array.IndexOf(headers, "Country Name");
+            Dictionary<string, GDPPopulation> dictonaryObject = new Dictionary<string, GDPPopulation>();
+            for (int i = 1; i < csvfile.Result.Length; i++)
+            {
+                try
+                {
+                    string[] row = CsvProcessed[i].Split(',');
+                    string countryName = row[countriesIndex];
+                    string nameOfContinent = Mapper[countryName];
+                    float Population = float.Parse(row[populationIndex]);
+                    float Gdp = float.Parse(row[gdpIndex]);
+                    try
+                    {
+                        dictonaryObject[nameOfContinent].GDP_2012 += Gdp;
+                        dictonaryObject[nameOfContinent].POPULATION_2012 += Population;
+                    }
+                    catch(Exception e) {
+
+                    }
+                    finally
+                    {
+                        GDPPopulation Object = new GDPPopulation() { GDP_2012 = Gdp, POPULATION_2012 = Population };
+                        dictonaryObject.Add(nameOfContinent, Object);
+                    }
+                }
+                catch(Exception e) {
+
+                }
+            }
+            await WriteFile(dictonaryObject);
+        }
     }
+
+    public class GDPPopulation
+    {
+        public float GDP_2012 { get; set; }
+        public float POPULATION_2012 { get; set; }
+    }
+
 }
+
+
